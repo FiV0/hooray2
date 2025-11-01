521d2ecaa6fd8ac95d4c2b17c9f853a5a181c06f
Finn Völkel <finn.volkel@gmail.com>
2025-11-01 23:08:45 +0100

    refactor

--- src/main/kotlin/org/hooray/iterator/BTreePrefixExtender.kt
+++ src/main/kotlin/org/hooray/iterator/BTreePrefixExtender.kt
@@ -1,40 +1,35 @@
 package org.hooray.iterator
 
 import me.tonsky.persistent_sorted_set.APersistentSortedSet
+import me.tonsky.persistent_sorted_set.Seq
 import org.hooray.algo.Extension
 import org.hooray.algo.Prefix
 import org.hooray.algo.PrefixExtender
 import org.hooray.util.IPersistentSortedMap
 
-sealed interface SealedIndex {
-    data class BTreeMap(val map: IPersistentSortedMap) : SealedIndex
-    data class BTreeSet(val set: APersistentSortedSet<Any, Any>) : SealedIndex
+sealed interface SealedBTreeIndex {
+    data class BTreeMap(val map: IPersistentSortedMap) : SealedBTreeIndex
+    data class BTreeSet(val set: APersistentSortedSet<Any, Any>) : SealedBTreeIndex
 }
 
 @Suppress("UNCHECKED_CAST")
-class BTreePrefixExtender(val index: SealedIndex, val participatesInLevel: List<Int>) : PrefixExtender {
+class BTreePrefixExtender(val bTreeIndex: SealedBTreeIndex, participatesInLevel: List<Int>) :
+    GenericPrefixExtender(when(bTreeIndex) {
+        is SealedBTreeIndex.BTreeMap -> SealedIndex.MapIndex(bTreeIndex.map)
+        is SealedBTreeIndex.BTreeSet -> SealedIndex.SetIndex(bTreeIndex.set as Set<Any>)
+    }, participatesInLevel) {
 
-    private fun internalPrefix(prefix: Prefix): Prefix {
-        val newPrefix = mutableListOf<Any>()
-        for (level in participatesInLevel) {
-            if (level < prefix.size) {
-                newPrefix.add(prefix[level])
-            }
-        }
-        return newPrefix
-    }
-
-    private fun indexFromPrefix(prefix: Prefix): SealedIndex {
-        var currentIndex = index
+    private fun indexFromPrefix(prefix: Prefix): SealedBTreeIndex {
+        var currentIndex = bTreeIndex
         val internalPrefix = internalPrefix(prefix)
 
         for (key in internalPrefix) {
             currentIndex = when (currentIndex) {
-                is SealedIndex.BTreeMap ->
-                    when(val newIndex = currentIndex.map.valAt(key)) {
-                        is IPersistentSortedMap -> SealedIndex.BTreeMap(newIndex)
-                        is APersistentSortedSet<*, *> -> SealedIndex.BTreeSet(newIndex as APersistentSortedSet<Any, Any>)
-                        else -> throw IllegalArgumentException("Unsupported value type in BTreeMap for key: $key")
+                is SealedBTreeIndex.BTreeMap ->
+                    when(val newIndex = currentIndex.map[key]) {
+                        is IPersistentSortedMap -> SealedBTreeIndex.BTreeMap(newIndex)
+                        is APersistentSortedSet<*, *> -> SealedBTreeIndex.BTreeSet(newIndex as APersistentSortedSet<Any, Any>)
+                        else -> throw IllegalArgumentException("Unsupported value type in BTreePrefixExtender for key: $key")
                     }
                 else -> throw IllegalArgumentException("Cannot index into a BTreeSet with a key")
             }
@@ -42,24 +37,19 @@ class BTreePrefixExtender(val index: SealedIndex, val participatesInLevel: List<
         return currentIndex
     }
 
-    override fun count(prefix: Prefix) =
-        when (val index= indexFromPrefix(prefix)) {
-            is SealedIndex.BTreeMap -> index.map.count()
-            is SealedIndex.BTreeSet -> index.set.size
+    override fun extend(prefix: Prefix, extensions: List<Extension>) : List<Extension> {
+        var seq = when (val index = indexFromPrefix(prefix)) {
+            is SealedBTreeIndex.BTreeMap -> index.map.seq() as Seq
+            is SealedBTreeIndex.BTreeSet -> index.set.seq() as Seq
         }
-
-    override fun propose(prefix: Prefix)  =
-        when (val index= indexFromPrefix(prefix)) {
-            is SealedIndex.BTreeMap -> index.map.iterator().asSequence().toList() as List<Extension>
-            is SealedIndex.BTreeSet -> index.set.iterator().asSequence().toList() as List<Extension>
+        val result = mutableListOf<Extension>()
+        for (ext in extensions) {
+            seq = seq.seek(ext)
+            if(seq.isEmpty()) break
+            if (seq.first() == ext) {
+                result.add(ext)
+            }
         }
-
-    // This is WCO (up to O(log(n))) as we start with the smallest extension list so
-    // extensions.size <= bTreeMap.size or bTreeSet.size
-    // Potentially we could optimize further by seeking on the BTree/Set instead of doing contains
-    override fun extend(prefix: Prefix, extensions: List<Extension>) =
-        when (val index = indexFromPrefix(prefix)) {
-            is SealedIndex.BTreeMap -> extensions.filter { ext -> index.map.containsKey(ext) }
-            is SealedIndex.BTreeSet -> extensions.filter { ext -> index.set.contains(ext) }
-        }
+        return result
+    }
 }
---
+++ src/main/kotlin/org/hooray/iterator/GenericPrefixExtender.kt
@@ -0,0 +1,62 @@
+package org.hooray.iterator
+
+import org.hooray.algo.Extension
+import org.hooray.algo.Prefix
+import org.hooray.algo.PrefixExtender
+
+sealed interface SealedIndex {
+    data class MapIndex(val map: Map<Any, Any>) : SealedIndex
+    data class SetIndex(val set: Set<Any>) : SealedIndex
+}
+
+@Suppress("UNCHECKED_CAST")
+open class GenericPrefixExtender(val index: SealedIndex, val participatesInLevel: List<Int>) : PrefixExtender {
+
+    protected fun internalPrefix(prefix: Prefix): Prefix {
+        val newPrefix = mutableListOf<Any>()
+        for (level in participatesInLevel) {
+            if (level < prefix.size) {
+                newPrefix.add(prefix[level])
+            }
+        }
+        return newPrefix
+    }
+
+    private fun indexFromPrefix(prefix: Prefix): SealedIndex {
+        var currentIndex = index
+        val internalPrefix = internalPrefix(prefix)
+
+        for (key in internalPrefix) {
+            currentIndex = when (currentIndex) {
+                is SealedIndex.MapIndex ->
+                    when(val newIndex = currentIndex.map[key]) {
+                        is HashMap<*, *> -> SealedIndex.MapIndex(newIndex as HashMap<Any, Any>)
+                        is HashSet<*> -> SealedIndex.SetIndex(newIndex as HashSet<Any> )
+                        else -> throw IllegalArgumentException("Unsupported value type in HashMap for key: $key")
+                    }
+                else -> throw IllegalArgumentException("Cannot index into a HashSet with a key")
+            }
+        }
+        return currentIndex
+    }
+
+    override open fun count(prefix: Prefix) =
+        when (val index= indexFromPrefix(prefix)) {
+            is SealedIndex.MapIndex -> index.map.size
+            is SealedIndex.SetIndex -> index.set.size
+        }
+
+    override open fun propose(prefix: Prefix)  =
+        when (val index= indexFromPrefix(prefix)) {
+            is SealedIndex.MapIndex -> index.map.keys.toList()
+            is SealedIndex.SetIndex -> index.set.toList()
+        }
+
+    // This is WCO as we start with the smallest extension list so
+    // extensions.size <= map.size or set.size
+    override open fun extend(prefix: Prefix, extensions: List<Extension>) =
+        when (val index = indexFromPrefix(prefix)) {
+            is SealedIndex.MapIndex -> extensions.filter { ext -> index.map.containsKey(ext) }
+            is SealedIndex.SetIndex -> extensions.filter { ext -> index.set.contains(ext) }
+        }
+}
