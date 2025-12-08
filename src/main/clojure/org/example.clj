889b3818f21763dfbdb76de622605249c9c78c1a
Finn Völkel <finn.volkel@gmail.com>
2025-12-08 11:06:01 +0100

    Remove code from clj-gradle init

--- src/main/clojure/org/example.clj
+++
@@ -1,6 +0,0 @@
-(ns org.example
-  (:import (org.example Library KotlinLibraryKt)))
-
-(comment
-  (Library/someLibraryMethod)
-  (KotlinLibraryKt/myFunction))
--- src/main/kotlin/org/example/KotlinLibrary.kt
+++
@@ -1,5 +0,0 @@
-package org.example
-
-fun myFunction() : String {
-   return "Hello, World!"
-}
