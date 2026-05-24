8da69da4efa7e105eebe791982018ee8399d31c1
Finn Völkel <finn.volkel@gmail.com>
2026-05-24 12:44:59 +0200

    Update README

--- src/test/clojure/hooray/subscription_test.clj
+++ src/test/clojure/hooray/subscription_test.clj
@@ -41,3 +41,46 @@
       (with-open [_subscription (h/subscribe fix/*node* names-query #(deliver delta %))]
         (h/transact fix/*node* [{:db/id :anna :name "Anna"}])
         (is-delta #{[["Anna"] 1]} (deref delta 1000 ::timeout))))))
+
+
+(comment
+  ;; README examples
+
+  (def node (h/connect {:type :mem :storage :hash :algo :generic}))
+  (h/transact node [{:db/id -100
+                     :db/ident :name
+                     :db/valueType :db.type/string
+                     :db/cardinality :db.cardinality/one}])
+
+  (def names-query
+    '{:find [name]
+      :where [[e :name name]]})
+
+  ;; Pull deltas explicitly.
+  (with-open [deltas (h/open-deltas node names-query)]
+    (h/transact node [{:db/id :ivan :name "Ivan"}])
+    (h/take! deltas))
+  ;; => ([["Ivan"] 1])
+
+  ;; Read deltas from a core.async channel.
+  (defn- take-with-timeout [ch]
+    (let [timeout (async/timeout 1000)
+          [value port] (async/alts!! [ch timeout])]
+      (if (= port timeout)
+        ::timeout
+        value)))
+
+  (let [delta-ch (h/delta-chan node names-query)]
+    (try
+      (h/transact node [{:db/id :petr :name "Petr"}])
+      (take-with-timeout delta-ch)
+      (finally
+        (async/close! delta-ch))))
+  ;; => ([["Petr"] 1])
+
+  ;; Receive deltas in a callback.
+  (with-out-str
+    (with-open [_subscription (h/subscribe node names-query println)]
+      (h/transact node [{:db/id :anna :name "Anna"}])))
+  ;; => "([[Anna] 1])\n"
+  )
