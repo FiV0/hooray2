4de495ec55e488dff577141a52007bafe1056a2b
Finn Völkel <finn.volkel@gmail.com>
2026-06-07 11:20:11 +0200

    datomic_semantics tweaks

--- dev/datomic_semantics.clj
+++ dev/datomic_semantics.clj
@@ -17,45 +17,72 @@
 (defn transact! [tx-data]
   @(d/transact *conn* tx-data))
 
+(t/use-fixtures :each with-conn)
+
 (deftest in-tx-transfer-test
-  (with-conn
-    (fn []
-      (transact! [{:db/ident       :user/email
-                   :db/valueType   :db.type/string
-                   :db/cardinality :db.cardinality/one
-                   :db/unique      :db.unique/identity}
-                  {:db/ident       :user/handle
-                   :db/valueType   :db.type/string
-                   :db/cardinality :db.cardinality/one
-                   :db/unique      :db.unique/identity}
-                  {:db/ident       :user/primary-friend
-                   :db/valueType   :db.type/ref
-                   :db/cardinality :db.cardinality/one
-                   :db/unique      :db.unique/identity}])
+  (transact! [{:db/ident       :user/email
+               :db/valueType   :db.type/string
+               :db/cardinality :db.cardinality/one
+               :db/unique      :db.unique/identity}
+              {:db/ident       :user/handle
+               :db/valueType   :db.type/string
+               :db/cardinality :db.cardinality/one
+               :db/unique      :db.unique/identity}
+              {:db/ident       :user/primary-friend
+               :db/valueType   :db.type/ref
+               :db/cardinality :db.cardinality/one
+               :db/unique      :db.unique/identity}])
 
-      (let [{:keys [tempids]} (transact! [{:db/id "alice"   :user/email          "alice"}
-                                          {:db/id "bob"     :user/handle         "bob"}
-                                          {:db/id "carol"   :user/primary-friend "bob"}])]
+  (let [{:keys [tempids]} (transact! [{:db/id "alice"   :user/email          "alice"}
+                                      {:db/id "bob"     :user/handle         "bob"}
+                                      {:db/id "carol"   :user/primary-friend "bob"}])]
 
-        ;; We upsert on :user/email and at the same time update the unique reference
-        ;; :user/primary-friend to "bob". This works because we retract the previous
-        ;; :user/primary-friend datom ["carol" :user/primary-friend "bob"]
-        (is (transact! [[:db/add     "u" :user/email          "alice"]
-                        [:db/add     "u" :user/primary-friend "f"]
-                        [:db/add     "f" :user/handle         "bob"]
-                        [:db/retract (get tempids "carol") :user/primary-friend (get tempids "bob")]]))))))
+    ;; We upsert on :user/email and at the same time update the unique reference
+    ;; :user/primary-friend to "bob". This works because we retract the previous
+    ;; :user/primary-friend datom ["carol" :user/primary-friend "bob"]
+    (is (transact! [[:db/add     "u" :user/email          "alice"]
+                    [:db/add     "u" :user/primary-friend "f"]
+                    [:db/add     "f" :user/handle         "bob"]
+                    [:db/retract (get tempids "carol") :user/primary-friend (get tempids "bob")]]))))
 
 (deftest tempid-only-in-value-pos
-  (with-conn
-    (fn []
-      (transact! [{:db/ident       :user/name
-                   :db/valueType   :db.type/string
-                   :db/cardinality :db.cardinality/one
-                   :db/unique      :db.unique/identity}
-                  {:db/ident       :user/follows
-                   :db/valueType   :db.type/ref
-                   :db/cardinality :db.cardinality/many}])
+  (transact! [{:db/ident       :user/name
+               :db/valueType   :db.type/string
+               :db/cardinality :db.cardinality/one
+               :db/unique      :db.unique/identity}
+              {:db/ident       :user/follows
+               :db/valueType   :db.type/ref
+               :db/cardinality :db.cardinality/many}])
 
-      (is (thrown? Exception
-                   (transact! [[:db/add "alice" :user/name "alice"]
-                               [:db/add "alice" :user/follows "bob"]]))))))
+  (is (thrown? Exception
+               (transact! [[:db/add "alice" :user/name "alice"]
+                           [:db/add "alice" :user/follows "bob"]]))))
+
+(def ^:private people-schema
+  [{:db/ident :name
+    :db/valueType :db.type/string
+    :db/cardinality :db.cardinality/one}
+   {:db/ident :age
+    :db/valueType :db.type/long
+    :db/cardinality :db.cardinality/one}])
+
+(deftest or-bound-mentioned-example
+  (transact! people-schema)
+
+  (transact! [{:name "Ivan" :age 25}
+              {:name "Bob" :age 35}])
+
+  (t/is (thrown? Exception
+                 (d/q '{:find [?e ?name]
+                        :where [[?e :name ?name]
+                                (or (and [?e :name "Bob"]
+                                         [?e :age ?age])
+                                    (and [?e :name "Ivan"]
+                                         [(< ?age 30)]))]}
+                      (d/db *conn*)))))
+
+(comment
+  (t/run-all-tests)
+  (t/run-test-var #'or-bound-mentioned-example)
+
+  )
