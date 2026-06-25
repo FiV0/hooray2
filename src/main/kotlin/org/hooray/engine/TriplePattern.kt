1e597c352ba12d8b22c9d2c4dfdf582140826740
Finn Völkel <finn.volkel@gmail.com>
2026-06-25 14:18:49 +0200

    partially rewritten TriplePattern (wip, not working)

--- src/main/kotlin/org/hooray/engine/TriplePattern.kt
+++ src/main/kotlin/org/hooray/engine/TriplePattern.kt
@@ -1,34 +1,94 @@
 package org.hooray.engine
 
+import java.lang.IllegalStateException
+import org.hooray.algo.Prefix
+import org.hooray.iterator.SealedIndex
+
 sealed interface PatternValue {
     data class Variable(val name: Any) : PatternValue
     data class Constant(val value: Any) : PatternValue
 }
 
+@Suppress("UNCHECKED_CAST")
 class TriplePattern(
     override val idx: Int,
-    private val eav: Map<*, *>,
-    private val aev: Map<*, *>,
-    private val ave: Map<*, *>,
-    private val entity: PatternValue,
-    private val attribute: PatternValue,
-    private val value: PatternValue,
+    override val variables: Set<Any>,
+    private val index: SealedIndex,
 ) : ExecPattern {
-    override val variables: Set<Any> = listOf(entity, attribute, value)
-        .mapNotNull { slot -> (slot as? PatternValue.Variable)?.name }
-        .toSet()
+
+    private fun relevantIntroduces(introduces: List<Any>): List<Any> {
+        return introduces.filter { it in variables }
+    }
+
+    private fun prefixIndexes(input: BindingSet) : List<Int> {
+        val res = mutableListOf<Int>()
+        for ((index, variable) in input.variables.withIndex()) {
+            if (variable in variables) {
+                res.add(index)
+            }
+        }
+        return res
+    }
+
+    private fun indexFromPrefix(prefix: Prefix, prefixIndexes: List<Int>): SealedIndex? {
+        var currentIndex : SealedIndex? = index
+
+        for (idx in prefixIndexes) {
+            val key = prefix[idx]
+            currentIndex = when (currentIndex) {
+                is SealedIndex.MapIndex ->
+                    when(val newIndex = currentIndex.map[key]) {
+                        is Map<*, *> -> SealedIndex.MapIndex(newIndex as Map<Any, Any>)
+                        is Set<*> -> SealedIndex.SetIndex(newIndex as Set<Any> )
+                        null -> null
+                        else -> throw IllegalArgumentException("Unsupported value type in HashMap for key: $key")
+                    }
+                else -> throw IllegalArgumentException("Cannot index into a HashSet with a key")
+            }
+        }
+        return currentIndex
+    }
+
+    private fun checkIntroduces(introduces: List<Any>) {
+        require(introduces.size == 1 || introduces.size == 2) {
+            "Triple pattern can only introduce one or two variables at a time"
+        }
+
+        require(variables.containsAll(introduces)) {
+            "Triple pattern cannot introduce variables it does not contain"
+        }
+    }
 
     override fun count(
         input: BindingSet,
         introduces: List<Any>,
         proposals: List<Proposal>,
     ): List<Proposal> {
-        if (!variables.containsAll(introduces)) {
-            return proposals
-        }
+        val relevantIntroduces = relevantIntroduces(introduces)
+        checkIntroduces(relevantIntroduces)
 
-        val counts = input.rows.map { row ->
-            matchingIntroductions(input.variables, row, introduces).size
+        val prefixIndexes = prefixIndexes(input)
+
+        val counts = input.rows.map { prefix ->
+            when (val index = indexFromPrefix(prefix, prefixIndexes)) {
+                null -> 0
+                is SealedIndex.MapIndex ->  when (relevantIntroduces.size) {
+                    1 -> index.map.size
+                    2 -> index.map.values.sumOf { value ->
+                        when (value) {
+                            is Map<*, *> -> value.size
+                            is Set<*> -> value.size
+                            else -> throw IllegalArgumentException("Unsupported value type in HashMap for key: $value")
+                        }
+                    }
+                    else -> throw IllegalArgumentException("Triple pattern can only introduce one or two variables at a time")
+                }
+                is SealedIndex.SetIndex -> when (relevantIntroduces.size) {
+                    1 -> index.set.size
+                    2 -> throw IllegalStateException("Triple pattern reached set where map was expected")
+                    else -> throw IllegalArgumentException("Triple pattern can only introduce one or two variables at a time")
+                }
+            }
         }
         return updateProposals(idx, proposals, counts)
     }
@@ -38,17 +98,47 @@ class TriplePattern(
         introduces: List<Any>,
         targetVariables: List<Any>,
     ): BindingSet {
-        require(variables.containsAll(introduces)) {
-            "Triple pattern cannot introduce variables it does not contain"
-        }
+        val relevantIntroduces = relevantIntroduces(introduces)
+        checkIntroduces(relevantIntroduces)
+
+        val prefixIndexes = prefixIndexes(input)
 
         val extensions = mutableListOf<RowExtension>()
-        input.rows.forEachIndexed { rowIndex, row ->
-            for (introducedValues in matchingIntroductions(input.variables, row, introduces)) {
-                extensions += RowExtension(rowIndex, introducedValues)
+        input.rows.forEachIndexed { rowIndex, prefix ->
+            when (val index = indexFromPrefix(prefix, prefixIndexes)) {
+                null -> {}
+                is SealedIndex.MapIndex ->  when (relevantIntroduces.size) {
+                    1 -> index.map.forEach { (key, value) ->
+                        val introducedValues = listOf(key)
+                        extensions += RowExtension(rowIndex, introducedValues)
+                    }
+                    2 -> index.map.values.sumOf { value ->
+                        when (value) {
+                            is Map<*, *> -> value.size
+                            is Set<*> -> value.size
+                            else -> throw IllegalArgumentException("Unsupported value type in HashMap for key: $value")
+                        }
+                    }
+                    else -> throw IllegalArgumentException("Triple pattern can only introduce one or two variables at a time")
+                }
+                is SealedIndex.SetIndex -> when (relevantIntroduces.size) {
+                    1 -> index.set.size
+                    2 -> throw IllegalStateException("Triple pattern reached set where map was expected")
+                    else -> throw IllegalArgumentException("Triple pattern can only introduce one or two variables at a time")
+                }
+            }
+
+
+            val index = indexFromPrefix(row, prefixIndexes)
+            when (index) {
+                null -> {}
+                is SealedIndex.SetIndex -> index.set.forEach { value ->
+                    val introducedValues = listOf(value)
+                    extensions += RowExtension(rowIndex, introducedValues)
+                }
             }
         }
-
+        // TODO reorder necessary?
         return input.extend(introduces, extensions).reorder(targetVariables)
     }
 
