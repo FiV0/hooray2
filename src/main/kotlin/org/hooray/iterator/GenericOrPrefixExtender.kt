319178a663e2bfddbe37fda9ed54ad8c818c5df3
Finn Völkel <finn.volkel@gmail.com>
2026-06-30 14:57:56 +0200

    simplify null handling in GenericOrPrefixExtender

--- src/main/kotlin/org/hooray/iterator/GenericOrPrefixExtender.kt
+++ src/main/kotlin/org/hooray/iterator/GenericOrPrefixExtender.kt
@@ -31,13 +31,11 @@ open class GenericOrPrefixExtender(val children: List<PrefixExtender>) : PrefixE
 
     override fun propose(prefix: Prefix): List<Extension> {
         val nodes = childTries.map { it.trieNodeFor(prefix) }
-        val childProposals = children.mapIndexed { index, extender -> if (nodes[index] != null) extender.propose(prefix) else null }
+        val childProposals = children.mapIndexed { index, extender -> if (nodes[index] != null) extender.propose(prefix) else emptyList()}
         for ((idx, proposals) in childProposals.withIndex()) {
-            if (proposals != null) {
-                val node = nodes[idx] ?: error("Node should not be null here")
-                for (proposal in proposals) {
-                    node.insert(proposal)
-                }
+            val node = nodes[idx] ?: continue
+            for (proposal in proposals) {
+                node.insert(proposal)
             }
         }
         // TODO the distinct call can likely be optimized to avoid large intermediate lists
@@ -48,10 +46,9 @@ open class GenericOrPrefixExtender(val children: List<PrefixExtender>) : PrefixE
         val nodes = childTries.map { it.trieNodeFor(prefix) }
         val result = mutableListOf<Extension>()
         for ((idx, child) in children.withIndex()) {
-            if (nodes[idx] == null) continue
+            val node = nodes[idx] ?: continue
             val childExtensions = child.intersect(prefix, extensions)
             for (extension in childExtensions) {
-                val node = nodes[idx] ?: error("Node should not be null here")
                 node.insert(extension)
             }
             result.addAll(childExtensions)
