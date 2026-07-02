f6fb1f59a6280f112f545909a2c8f9ccd685ba4c
Finn Völkel <finn.volkel@gmail.com>
2026-07-02 17:21:49 +0200

    update LevelParticipation

--- src/main/kotlin/org/hooray/iterator/LevelParticipation.kt
+++ src/main/kotlin/org/hooray/iterator/LevelParticipation.kt
@@ -1,5 +1,14 @@
 package org.hooray.iterator
 
 interface LevelParticipation {
+    // Returns a list of levels in which the participant variables are involved
+    fun variableLevels(): List<Int>
+    // Returns true if the participant is involved in the specified level
+    // This returns true for a subset of variable levels, but not necessarily all of them
+    // For example:
+    //    [(= x y)]
+    // variableLeves() = [1 2]
+    // participatesInLevel(1) = false
+    // participatesInLevel(2) = true
     fun participatesInLevel(level: Int): Boolean
 }
